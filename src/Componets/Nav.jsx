f2f480380bfd03e7c825cc828499fe1d3de4962a
jose <hernandezjos1121@gmail.com>
2023-07-08 19:44:33 -0400

    push

--- src/Componets/Nav.jsx
+++ src/Componets/Nav.jsx
@@ -1,24 +1,39 @@
 import React from "react";
-import "../universal.css" 
-import "./Nav.css" 
+import "../universal.css";
+import "./Nav.css";
+import { Link } from "react-router-dom";
 export const Nav = () => {
   return (
-    <div className="container">
-      <div className="row">
-        <nav>
-          <div className="site-title">
-            <h2>
-              AnimeCharacters
-            </h2>
-          </div>
-          <div className="site-links-wrapper">
-            <ul className="site-links">
-              <li className="site-link">About</li>
-              <li className="site-link">Contact</li>
-            </ul>
-          </div>
-        </nav>
+    <section id="NAV">
+      <div className="container">
+        <div className="row">
+          <nav>
+            <div className="site-title">
+              <h2>AnimeCharacters</h2>
+            </div>
+            <div className="site-links-wrapper">
+              <ul className="site-links">
+                <Link to={"/"}>
+                  <li className=" site-link">Home</li>
+                </Link>
+                <li
+                  onClick={() => alert("Ive Suffered Enough")}
+                  className="no site-link"
+                >
+                  Contact
+                </li>
+
+                <li
+                  onClick={() => alert("Ive Suffered Enough")}
+                  className="no site-link"
+                >
+                  About
+                </li>
+              </ul>
+            </div>
+          </nav>
+        </div>
       </div>
-    </div>
+    </section>
   );
 };
